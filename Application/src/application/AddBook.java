2afd9a770c508027a8216931569beea7631bb6c4
dnlsj <80169871+dnlsj@users.noreply.github.com>
2021-05-02 19:35:32 +0200

    Update AddBook.java

--- Application/src/application/AddBook.java
+++ Application/src/application/AddBook.java
@@ -118,7 +118,7 @@ public class AddBook extends javax.swing.JFrame{
     }// </editor-fold>//GEN-END:initComponents
 
     private void b1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b1ActionPerformed
- /*A hozzáadás gomb megnyomásával kapcsolat létesítési próba az adatbázissal, amennyiben sikerült akkor a t1-t4 mezőkbe bevitt adatok feltöltése az adatbázisba  */      
+ /*A hozzáadás gomb megnyomásával kapcsolat létesítési próba az adatbázissal, amennyiben sikerült akkor a t1-t4 mezőkbe bevitt adatok feltöltése az adatbázisba*/      
         try{
             conn con = new conn();
             if(evt.getSource() == b1){
