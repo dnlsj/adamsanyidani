a8fc4242e0fd14831514101d50600459d1ea261b
dnlsj <80169871+dnlsj@users.noreply.github.com>
2021-05-02 19:33:33 +0200

    Update AddStudent.java

--- Application/src/application/AddStudent.java
+++ Application/src/application/AddStudent.java
@@ -155,7 +155,7 @@ public class AddStudent extends javax.swing.JFrame {
                
 		int i = st.executeUpdate();
  /*IF elággal megoldjuk, hogy amennyiben sikeres az adatok update-elése akkor a Hozzáadva üzenet jelenjen meg, amennyiben sikertelen akkor Hiba üzenet.
- Sikeres update-elés esetén az ablak bezárul és megnyitja a Home ablakot*/
+ Sikeres update-elés esetén az ablak bezárul és megnyitja a Home ablakot */
 		if (i > 0){
                     JOptionPane.showMessageDialog(null, "Hozzáadva");
                     this.setVisible(false);
